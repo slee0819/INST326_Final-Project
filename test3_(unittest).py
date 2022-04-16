a6f28b8d126266543a4fd3d74133c8106d14ba13
slee0819 <102753338+slee0819@users.noreply.github.com>
2022-04-15 20:03:49 -0400

    Create test3_(unittest).py
    we wrote the test code that intentionally throws an error.
    The test to check the error uses the assertRaises() method and wraps it in a "with" statement to check whether the internal code generates an error.

---
+++ test3_(unittest).py
@@ -0,0 +1,47 @@
+import unittest
+import os
+
+
+def custom_function(file_name):
+    with open(file_name, 'rt') as f:
+        return sum(1 for _ in f)
+
+
+# Write the Testcase
+class CustomTests(unittest.TestCase):
+
+    def setUp(self):
+        """Write the file before the test starts"""
+        self.file_name = 'test_file.txt'
+        with open(self.file_name, 'wt') as f:
+            f.write("""
+            Hello
+            world
+            Thanks!
+            """.strip())
+
+    def tearDown(self):
+        """ After test delete the file"""
+        try:
+            os.remove(self.file_name)
+        except:
+            pass
+
+    def test_runs(self):
+        """Test method to determine whether simple execution"""
+
+        custom_function(self.file_name)
+
+    def test_line_count(self):
+        self.assertEqual(custom_function(self.file_name), 3)
+
+# write test code that intentionally throws an error.
+class CustomTests(unittest.TestCase):
+
+    def test_no_file(self):
+        with self.assertRaises(IOError):
+            custom_function(self.file_name)
+
+# Start the unittest
+if __name__ == '__main__':
+    unittest.main()
