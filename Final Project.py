d348c15840857af99f08c4ca7340b20863ce1b24
zznnyy <70858876+zznnyy@users.noreply.github.com>
2022-05-13 00:47:58 -0400

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -1,9 +1,31 @@
 # Team 11:58Pm( Sang Hwa Lee, Ningyuan Zhang, Miguel Rodriguez)
 # INST326 - Final Project 
-
 import sqlite3
 conn = sqlite3.connect('Final Project.db')
-conn.close()
+cursor = conn.cursor()
+sql = """
+CREATE TABLE IF NOT EXISTS phone (
+	"Phone"	TEXT,
+	"Name"	TEXT,
+	"Email"	TEXT,
+	"Address" TEXT
+);
+"""
+cursor.execute(sql)
+conn.commit()
+
+class Contact:
+    def __init__(self, name, phoneNum, email, address):
+        self.name = name
+        self.phoneNum = phoneNum
+        self.e_mail = email
+        self.addr = address
+
+    def print_info(self):
+        print("Name: ", self.name)
+        print("Phone Number: ", self.phoneNum)
+        print("E-mail: ", self.email)
+        print("Address: ", self.address)
 
 def insert():
     """Inserts a contact to the contact list.
@@ -17,37 +39,37 @@ def insert():
         Creates a contact object 
         Prints information to the console
     """
-    
-    phoneNum = input('Enter Phone Number:\n')
-    name = input('Enter Name:\n')
-    email = input('Enter Email:\n')
-    address = input('Enter Address:\n')
-    sql1 = 'insert into MT(Phone,NAME,EMAIL,ADDRESS,ADDRESS)'
-    sql1 += 'values("%d","%s","%d","%s");'%(phoneNum,name,email,address)
-    conn.execute(sql1)
+    phoneNum = input('Enter Phone Number: ')
+    name = input('Enter Name: ')
+    email = input('Enter Email: ')
+    address = input('Enter Address: ')
+    sql1 = 'insert into phone(Phone,Name,Email,Address)'
+    sql1 += ' values("%s","%s","%s","%s");' % (phoneNum, name, email, address)
+    cursor.execute(sql1)
     conn.commit()
-    print ("Records insert successfully")
- 
+    print("Records insert successfully\n")
+
+
 def delete():
     """Deletes a contact from the contact list.
     
     Args:
         name: full name of the contact
-
     Side Effects:
         Deletes the contact object
         Prints information to the console
     """
-    name = input("Enter the name you wish to delete:")
-    cursor = conn.execute("SELECT name from MT where name = '%s';"%name)
-    for row in cursor:
+    name = input("Enter the name you wish to delete: ")
+    query = cursor.execute("SELECT name from phone where Name = '%s';" % name)
+    for row in query:
         if name == row[0]:
-            conn.execute("DELETE from MT where name = '%s';"%name)
+            cursor.execute("DELETE from phone where Name = '%s';" % name)
             conn.commit()
-            print ("Records delete successfully")
+            print("Records delete successfully\n")
             break
     else:
-        print ("Sorry,this user is not exist")
+        print("Sorry, this user is not exist\n")
+
 
 def modify():
     """Modifys a contact from the contact list
@@ -62,26 +84,30 @@ def modify():
         Modifys the contact information
         Prints information to the console
     """
-    name = input("Enter the name you wish to change:")
-    sql4 = "phoneNum, name, email, address from MT where name = '%s';"%name
-    cursor = conn.execute(sql4)
-    x = input("Enter the phone number you wish to change:")
-    y = input("Enter the email you wish to change:")
-    z = input("Enter the address you wish to change:")
-    sql3 = "UPDATE MT set phone number = '%s',email = '%d',\
-        address = '%d' where name = '%s';"%(x,y,z,name)
-    conn.execute(sql3)
-    conn.commit()
-    print ("Update complete")
-    sql5 = "SELECT phoneNum, name, email, address from MT where name = '%s';"%name
-    cursor = conn.execute(sql5)
-    for row in cursor:
-        print ("phoneNum", row[0])
-        print ("name = ", row[1])
-        print ("email = ",row[2])
-        print ("address = ", row[3]),"\n"
+    name = input("Enter the name you wish to change: ")
+    sql4 = "SELECT * from phone where Name = '%s';" % name
+    query = cursor.execute(sql4)
+    for row in query:
+        if name == row[1]:
+            x = input("Enter the phone number you wish to change: ")
+            y = input("Enter the email you wish to change: ")
+            z = input("Enter the address you wish to change: ")
+            sql3 = "UPDATE phone set Phone = '%s', Email = '%s', Address = '%s' where Name = '%s';" % (x, y, z, name)
+            cursor.execute(sql3)
+            conn.commit()
+            print("Update complete")
+            sql5 = "SELECT * from phone where name = '%s';" % name
+            query = cursor.execute(sql5)
+            for row in query:
+                print("phoneNum = ", row[0])
+                print("name = ", row[1])
+                print("email = ", row[2])
+                print("address = ", row[3])
+                print()
+            break
+    else:
+        print("Sorry, this user is not exist\n")
 
-conn = sqlite3.connect('Final Project.db')
 
 def search():
     """Searches a contact from the contact list
@@ -93,18 +119,19 @@ def search():
         Prints information to the console
     
     """
-    conn = sqlite3.connect('Final Project.db')
-    name = input('Enter the name you want to search')
-    sql2 = "SELECT phoneNum, name, email, address from MT where name = '%s';" % (name)
-    cursor = conn.execute(sql2)
-    for row in cursor:
-        print ("phoneNum", row[0])
-        print ("name = ", row[1])
-        print ("email = ",row[2])
-        print ("address = ", row[3]), "\n"
+    name = input('Enter the name you want to search: ')
+    sql2 = "SELECT * from phone where name = '%s';" % name
+    query = cursor.execute(sql2)
+    for row in query:
+        print("phoneNum = ", row[0])
+        print("name = ", row[1])
+        print("email = ", row[2])
+        print("address = ", row[3])
+        print()
         break
     else:
-        print ("Sorry, this contact does not exist")
+        print("Sorry, no information\n")
+
 
 def showall():
     """Show all the contacts in the contact list
@@ -112,27 +139,35 @@ def showall():
     Side Effects:
         Prints information to the console
     """
-    cursor = conn.execute("SELECT phoneNum, name, email, address from MT")
-    for row in cursor:
-        print ("phoneNum", row[0])
-        print ("name = ", row[1])
-        print ("email = ",row[2])
-        print ("address = ", row[3]), "\n"
-    print ("Complete")
-    cursor = conn.execute("select count(*) from MT;")
-    for row in cursor:
-        print ("A total of %d user")%row[0]
+    query = cursor.execute("SELECT count(*) from phone")
+    for row in query:
+        print("A total of %d user\n" % row[0])
+
+    query = cursor.execute("SELECT * from phone")
+    for row in query:
+        print("phoneNum = ", row[0])
+        print("name = ", row[1])
+        print("email = ", row[2])
+        print("address = ", row[3])
+        print()
+    print("Complete\n")
+
 
 def menu():
-    print ('1.Add Contact')
-    print ('2.Delet Contact')
-    print ('3.Modify Contact')
-    print ('4.Search Contact')
-    print ('5.Show All Contact')
-    print ('6.Exit')
+    """Display menu
+
+    Side Effects:
+        Show all the menu options
+    """
+    print('1.Add Contact')
+    print('2.Delete Contact')
+    print('3.Modify Contact')
+    print('4.Search Contact')
+    print('5.Show All Contact')
+    print('6.Exit')
 while True:
     menu()
-    x = input('Enter a Number:')
+    x = input('Enter a Number: ')
     if x == '1':
         insert()
         continue
@@ -149,9 +184,11 @@ while True:
         showall()
         continue
     if x == '6':
-        print ("Thanks for using!")
+        print("Thanks for using!")
         exit()
-        continue
     else:
-        print ("This option is not exits, enter again")
-        continue
+        print("This option is not exits, enter again")
+        break
+
+if __name__ == "__main__":
+    menu()
